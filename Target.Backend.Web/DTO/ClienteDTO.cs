b9549cfc59b673663fc2afa9598859bbb61f7df8
ramon159 <ramonarthur14@gmail.com>
2022-01-04 00:07:44 -0300

    feat(DTO): Cliente e ClienteEndereco adicionados com validações

---
+++ Target.Backend.Web/DTO/ClienteDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Target.Backend.Web.DTO
+{
+    public class ClienteDTO
+    {
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [StringLength(150, MinimumLength = 5, ErrorMessage = "Este campo deve conter entre {2} a {1} caracteres")]
+        public string NomeCompleto { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [DataType(DataType.DateTime, ErrorMessage = "Este campo deve ter o formato de data do tipo JSON")]
+        public DateTime DataNascimento { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        // adicionar validação customizada
+        public string CPF { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(1, 999999.99, ErrorMessage = "A renda mensal deve ser entre {1} a {2} R$")]
+        public decimal RendaMensal { get; set; }
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        public ClienteEnderecoDTO Endereco { get; set; }
+    }
+}
---
+++ Target.Backend.Web/DTO/ClienteEnderecoDTO.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Target.Backend.Web.DTO
+{
+    public class ClienteEnderecoDTO
+    {
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Este campo deve conter entre {2} a {1} caracteres")]
+        public string Logradouro { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Este campo deve conter entre {2} a {1} caracteres")]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Este campo deve conter entre {2} a {1} caracteres")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Este campo deve conter entre {2} a {1} caracteres")]
+        public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "Este campo deve conter exatamente {1} caracteres")]
+        public string UF { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        //adicionar validação customizada
+        public string CEP { get; set; }
+    }
+}
