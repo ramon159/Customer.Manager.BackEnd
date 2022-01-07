b18e923a6c9703b419d48191e59a589ba9a66b77
ramon159 <ramonarthur14@gmail.com>
2022-01-07 18:22:58 -0300

    test: teste de integração do EstadoRepository criado

---
+++ Target.Backend.Tests.Integration/Repositories/EstadoRepositoryTests.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using System;
+using System.Threading.Tasks;
+using Target.Backend.Web.Interfaces.Repositories;
+using Target.Backend.Web.Repositories;
+using Xunit;
+
+namespace Target.Backend.Tests.Integration.Repositories
+{
+    public class EstadoRepositoryTests
+    {
+        private IEstadoRepository _estadoRepository;
+
+        public EstadoRepositoryTests()
+        {
+            _estadoRepository = new EstadoRepository();
+        }
+
+        [Fact]
+        public async Task GetCidadesByUFAsync_UF_ShouldReturnsObject()
+        {
+            // Arrange
+            string UF = "RJ";
+
+            // Act
+            var result = await _estadoRepository.GetCidadesByUFAsync(UF);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task GetEstadosAsync_ShouldReturnsObject()
+        {
+            // Act
+            var result = await _estadoRepository.GetEstadosAsync();
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
